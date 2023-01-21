78052674406d03e61c12f3fc9e3150116fab7634
Meghanamurthyy <meghanamurthy121@gmail.com>
2023-01-21 19:46:41 +0530

    To perform arithmetic operations on entered two numbers

---
+++ src/main/kotlin/Calci.kt
@@ -0,0 +1,15 @@
+fun main()
+{
+    println("Enter the choice: 1.Addition 2.Subtraction 3.Multiplication 4.Division 5.Exit")
+    val ch= readLine()?.toInt()!!
+    println("Enter two numbers for operation")
+    val a= readLine()?.toDouble()!!
+    val b= readLine()?.toDouble()!!
+        when (ch) {
+            1 -> println("Result after addition is ${a + b}")
+            2 -> println("Result after subtraction is ${a - b}")
+            3 -> println("Result after multiplication is ${a * b}")
+            4 -> println("Result after division is ${a / b}")
+
+        }
+}
