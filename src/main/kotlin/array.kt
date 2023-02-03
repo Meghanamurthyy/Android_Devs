f2f06e0a8e7236a9c14306aadc53814e5a6de547
Meghanamurthyy <meghanamurthy121@gmail.com>
2023-02-03 20:39:57 +0530

    Solving a online problem statement

--- src/main/kotlin/array.kt
+++ src/main/kotlin/array.kt
@@ -1,11 +1,27 @@
-fun main()
-{
-    var myarray= arrayOf(1,2,3,4,5,6,7)
-    var sum=0
-    for(i in myarray)
+//This program asks for a password, and if it is shorter than 5 characters, it asks again until it receives a password of 5 or longer.
+//We need to decompose the program to the functions validatePassword() and main(). You already have the main() function.
+//Your task is to write a password length check in the validatePassword() function.
+// If the password is less than 5 characters, print "Your password should be longer than five characters, please write a new password" and return false as the result of the function.
+//If the password is 5 characters or longer, print "Good password" and return true
+fun validatePassword(password: String): Boolean {
+    // write your code here
+    val n = password.length>4
+    if(!n)
     {
-        sum=sum+i
-
+        println("Your password should be longer than five characters, please write a new password")
     }
-    println(sum)
+    else {
+        println("Good password")
+    }
+    return n
+
+}
+
+// do not change the code below
+fun main() {
+    var validationResult = false
+    while (!validationResult) {
+        val password = readln()
+        validationResult = validatePassword(password)
+    }
 }
